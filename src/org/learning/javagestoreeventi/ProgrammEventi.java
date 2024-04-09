bf461d14a820f9e127a5ab8a859ccb3a28d97955
DavideGila <davide.gila@gmail.com>
2024-04-09 16:29:34 +0200

    created al necessary functions, milestone 4 completed

--- src/org/learning/javagestoreeventi/ProgrammEventi.java
+++ src/org/learning/javagestoreeventi/ProgrammEventi.java
@@ -1,6 +1,11 @@
 package org.learning.javagestoreeventi;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProgrammEventi {
     // ATTRIBUTI
@@ -8,9 +13,9 @@ public class ProgrammEventi {
     private List<Evento> eventi;
 
     // COSTRUTTORI
-    public ProgrammEventi(String titolo, List<Evento> eventi) {
+    public ProgrammEventi(String titolo) {
         this.titolo = titolo;
-        this.eventi = eventi;
+        this.eventi = new ArrayList<>();
     }
 
     // GETTER E SETTER
@@ -31,4 +36,37 @@ public class ProgrammEventi {
     }
 
     // METODI
+    public void aggiungiEvento(Evento evento) {
+        eventi.add(evento);
+    }
+
+    public List<Evento> eventiInData(LocalDate data) {
+        return eventi.stream()
+                .filter(e -> e.getDate().equals(data))
+                .collect(Collectors.toList());
+    }
+
+    public int numeroEventi() {
+        return eventi.size();
+    }
+
+    public void svuotaEventi() {
+        eventi.clear();
+    }
+
+    public String elencoEventiOrdinatiPerData() {
+        // ordina gli eventi per data
+        eventi.sort(Comparator.comparing(Evento::getDate));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Titolo del programma: ").append(titolo).append("\n");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        for (Evento evento : eventi) {
+            String dataFormattata = evento.getDate().format(formatter);
+            stringBuilder.append(dataFormattata).append(" - ").append(evento.getTitle()).append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
 }
