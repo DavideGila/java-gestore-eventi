c2c87dd2415ba0b54e3b5da3cd13c1091e165fbf
DavideGila <davide.gila@gmail.com>
2024-04-09 18:06:58 +0200

    added some methods created in the class ProgrammEventi

--- src/org/learning/javagestoreeventi/Main.java
+++ src/org/learning/javagestoreeventi/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ProgrammEventi programmaEventi = new ProgrammEventi("Programma Eventi");
         try {
             System.out.println("Creazione di un nuovo evento.");
             System.out.print("Inserisci il nome dell'evento: ");
@@ -19,6 +19,37 @@ public class Main {
             int capacity = Integer.parseInt(scanner.nextLine());
 
             Evento event = new Evento(title, date, capacity);
+
+            // utilizzo il metodo aggiungiEvento
+            programmaEventi.aggiungiEvento(event);
+            programmaEventi.salvaEventi("eventi.txt");
+
+            // utilizzo il metodo numeroEventi per vedere il numero di eventi presenti
+            System.out.println("Numero totale di eventi nel programma: " +
+                    programmaEventi.numeroEventi("eventi.txt"));
+
+            // chiedo all'utente se vuole svuotare la lista degli eventi presenti
+            System.out.print("Vuoi svuotare tutti gli eventi disponibili? (s/n): ");
+            String responseEmpty = scanner.nextLine();
+            if ("s".equalsIgnoreCase(responseEmpty)) {
+                programmaEventi.svuotaEventi();
+                System.out.println("Tutti gli eventi sono stati svuotati.");
+            } else {
+                System.out.println("Operazione annullata.");
+            }
+
+            // utilizzo nuovamente il metodo numeroEventi() per farmi ritornare quanti eventi ci sono dopo
+            // l'operazione effettuata in precedenza
+            System.out.println("Numero totale di eventi nel programma dopo l'operazione: "
+                    + programmaEventi.numeroEventi("eventi.txt"));
+
+            if (programmaEventi.numeroEventi("eventi.txt") == 0) {
+                System.out.println("Non ci sono eventi disponibili.");
+                scanner.close();
+                System.exit(0);
+            }
+
+            System.out.println(programmaEventi.elencoEventiOrdinatiPerData());
 
             System.out.print("Vuoi effettuare delle prenotazioni? (s/n): ");
             String responseReservation = scanner.nextLine();
@@ -57,7 +88,7 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Errore: " + e.getMessage());
         }
-        
+
         scanner.close();
     }
 }
